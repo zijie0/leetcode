cc92ea18095377e29d2d888eb895256aaae2102b
zijie0 <myoilbox@gmail.com>
2016-04-26 19:53:49 +0800

    add new solution

---
+++ Java/src/com/zijie/IsSameTree.java
@@ -0,0 +1,20 @@
+package com.zijie;
+
+/**
+ * Created by zijie.zy on 2016/4/26.
+ */
+public class IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p != null && q != null) {
+            if (p.val != q.val) {
+                return false;
+            } else {
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            }
+        } else {
+            return false;
+        }
+    }
+}
